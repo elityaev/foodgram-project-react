18d8ff8846af7215457420141ca53c563ca6d992
Евгений Литяев <elityaev@yandex.ru>
2022-06-20 08:58:12 +0300

    Отправка на ревью 2

---
+++ backend/entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+echo "Start makemigrations"
+python manage.py makemigrations /dev/null 2>&1;
+echo "Start migrate"
+python manage.py migrate /dev/null 2>&1;
+echo "Start loadin db"
+python manage.py db_load /dev/null 2>&1;
+echo "Start wsgi"
+gunicorn yatube.wsgi:application --bind 0.0.0.0:8000 --error-logfile /code/logs/gunicorn.error.log --access-logfile /code/logs/gunicorn.access.log --capture-output --log-level debug
+exec "$@"
